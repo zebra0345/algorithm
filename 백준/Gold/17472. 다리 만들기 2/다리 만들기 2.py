75f2a7e5c54c73c588ed727ff74af8960f67daf2
신우진 <116381280+zebra0345@users.noreply.github.com>
2026-03-20 18:50:17 +0900

    [Gold I] Title: 다리 만들기 2, Time: 112 ms, Memory: 112312 KB -BaekjoonHub

--- 백준/Gold/17472. 다리 만들기 2/다리 만들기 2.py
+++ 백준/Gold/17472. 다리 만들기 2/다리 만들기 2.py
@@ -1,73 +1,78 @@
+# y, x
 N, M = map(int, input().split())
 
-land = [list(map(int, input().split())) for _ in range(N)]
-visited = [[False] * M for _ in range(N)]
 
-# 좌 우 상 하
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
+land = [list(map(int, input().split())) for _ in range(N)]
 
 from collections import deque
-def bfs(start_y, start_x, island_id):
-    q = deque([(start_y, start_x)])
-    
-    land[start_y][start_x] = island_id
-    visited[start_y][start_x] = True
-    
+from math import degrees
+
+visited = [[0] * M for _ in range(N)]
+
+dy = [-1, 1, 0, 0]
+dx = [0, 0, -1, 1]
+
+q = deque()
+
+def check(y, x, land_cnt):
+    q.append((y, x))
+    visited[y][x] = 1
+    land[y][x] = land_cnt
     while q:
         y, x = q.popleft()
         
-        for i in range(4):
-            ny, nx = y + dy[i], x + dx[i]
+        for d in range(4):
+            ny = y + dy[d]
+            nx = x + dx[d]
             
-            if 0 <= ny < N and 0 <= nx < M:
-                if not visited[ny][nx] and land[ny][nx] == 1:
-                    visited[ny][nx] = True
-                    land[ny][nx] = island_id
-                    q.append((ny, nx))
+            if 0 <= ny < N and 0 <= nx < M and visited[ny][nx] == 0 and land[ny][nx] != 0:
+                q.append((ny, nx))
+                visited[ny][nx] = 1
+                land[ny][nx] = land_cnt
 
-island_id = 1
-
+land_cnt = 1
 for i in range(N):
     for j in range(M):
-        if not visited[i][j] and land[i][j] == 1:
-            bfs(i, j, island_id)
-            island_id += 1
-num_islands = island_id - 1
+        if land[i][j] != 0 and visited[i][j] == 0:
+            check(i, j, land_cnt)
+            land_cnt += 1
+
+land_cnt -= 1
 edge = set()
 
-for i in range(N):
-    for j in range(M):
-        if land[i][j] > 0:
-            current_id = land[i][j]
-            
+for y in range(N):
+    for x in range(M):
+        if land[y][x] > 0:
+            current_id = land[y][x]
             for d in range(4):
-                ny, nx = i + dy[d], j + dx[d]
-                dist = 0
+                ny = y + dy[d]
+                nx = x + dx[d]
+                cost = 0
+                
                 while 0 <= ny < N and 0 <= nx < M:
-                    if land[ny][nx] == current_id:
+                    if current_id == land[ny][nx]:
                         break
                     
                     elif land[ny][nx] == 0:
-                        dist+=1
                         ny += dy[d]
                         nx += dx[d]
-                    
+                        cost += 1
                     else:
-                        target = land[ny][nx]
-                        if dist >= 2:
-                            edge.add((dist, current_id, target))
+                        if cost >= 2:
+                            target = land[ny][nx]
+                            edge.add((cost, current_id, target))
                         break
 
 edge = list(edge)
 edge.sort()
 
-parent =[i for i in range(num_islands+1)]
+parent = [i for i in range(land_cnt+1)]
+
 
 def find(x):
     if parent[x] != x:
         parent[x] = find(parent[x])
-    
+        
     return parent[x]
 
 def union(a, b):
@@ -76,17 +81,20 @@ def union(a, b):
     
     if a != b:
         parent[a] = b
-        
+        return False
+    
+    return True
+
 total_cost = 0
 total_bridge = 0
-
-for dist, a, b in edge:
+for c, a, b in edge:
     if find(a) != find(b):
         union(a, b)
-        total_cost += dist
+        total_cost += c
         total_bridge += 1
+        
 
-if total_bridge == num_islands - 1:
+if total_bridge == land_cnt - 1:
     print(total_cost)
 
 else:
